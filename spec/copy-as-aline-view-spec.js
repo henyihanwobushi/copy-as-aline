b79fcbc7abdca38069f23b8cde8564ede645ad91
work <work@workdeMacBook-Pro.local>
2019-04-18 20:41:55 +0800

    first ci

---
+++ spec/copy-as-aline-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CopyAsAlineView from '../lib/copy-as-aline-view';
+
+describe('CopyAsAlineView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
